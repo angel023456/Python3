dfca256ec4478d26c54ecbf9bdb29e2db534b49c
angel0234 <160226642+angel023456@users.noreply.github.com>
2024-06-10 21:18:55 +0200

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,26 @@
+edad = 23
+if edad >=18:
+    print('es mayor de edad')
+else:
+    print ('es menor de edad')
+    
+    
+
+
+# Inicializa num con un valor
+num = 5  # Puedes cambiar este valor para probar con diferentes números
+
+# Verifica si num es mayor o igual a 0
+if num >= 0:
+    # Bucle que continúa mientras num sea menor que 10
+    while num < 10:
+        # Imprime el valor actual de num
+        print(num)
+        # Incrementa num en 1
+        num += 1
+        
+        if 'condicion' and 'codicion2' and 'condion3' and \
+            'condicion4' and 'condicion5':
+
+
+                dias  = ['lunes', 'martes','mercoles']
